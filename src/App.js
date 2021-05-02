d0b0bb9679898fb6f2054566b844e79fbe1bb0a3
Primavera19476 <78546579+Primavera19476@users.noreply.github.com>
2021-05-02 17:47:23 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+    <div className="container">
      <h1>Weather App</h1>
      <footer>
        This website was coded by{" "}
@@ -10,6 +11,7 @@ export default function App() {
         {" "}and is open-sourced on{" "}
      <a href="https://github.com/Primavera19476/react-weather-app-2" target="_blank">GitHub</a>
      </footer>
+     </div>
     </div>
   );
 }
