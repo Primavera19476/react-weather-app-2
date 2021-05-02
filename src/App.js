6bbc751c2b206e7408a5b09f68ce31393e457074
Primavera19476 <78546579+Primavera19476@users.noreply.github.com>
2021-05-02 13:48:54 +0200

    Added GitHub Linkout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+     <footer>
+       This website was coded by{" "}
+     <a href="https://www.linkedin.com/in/clararotsch/" target="_blank">Clara Rotsch</a>  
+        {" "}and is open-sourced on{" "}
+     <a href="https://github.com/Primavera19476/react-weather-app-2" target="_blank">GitHub</a>
+     </footer>
     </div>
   );
 }
